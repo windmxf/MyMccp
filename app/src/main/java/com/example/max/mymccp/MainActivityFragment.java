22fd53c2040dad2f3ba333f73c883988d88565f8
maxf <75807638@qq.com>
2016-04-06 22:06:03 +0800

    格式化
    格式化

--- app/src/main/java/com/example/max/mymccp/MainActivityFragment.java
+++ app/src/main/java/com/example/max/mymccp/MainActivityFragment.java
@@ -25,13 +25,14 @@ public class MainActivityFragment extends Fragment {
     GridView gridView;
     List Osteps = new ArrayList<>();
     List Xsteps = new ArrayList();
-    List<Integer> allSteps=new ArrayList<Integer>();
+    List<Integer> allSteps = new ArrayList<Integer>();
     int count = 0;
     Button reset;
     ArrayAdapter<String> adapter;
-    int numCols=19;
-    int numInLine=5;
-    boolean over=false;
+    int numCols = 19;
+    int numInLine = 5;
+    boolean over = false;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -45,7 +46,7 @@ public class MainActivityFragment extends Fragment {
             gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
                 public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                    if (over){
+                    if (over) {
                         return;
                     }
                     TextView t = (TextView) view.findViewById(R.id.item);
@@ -55,7 +56,7 @@ public class MainActivityFragment extends Fragment {
                             / numCols) + 1);
 
                     int[] thisStep = {x, y};
-                    if (allSteps.contains(i)){
+                    if (allSteps.contains(i)) {
                         return;
                     }
                     if (count % 2 == 0) {
@@ -68,20 +69,20 @@ public class MainActivityFragment extends Fragment {
                     allSteps.add(i);
                     t.setText(sel);
                     count++;
-                    if (count > ((numInLine-1)*2) && isLine(sel, x, y)) {
+                    if (count > ((numInLine - 1) * 2) && isLine(sel, x, y)) {
                         Toast.makeText(getActivity(), sel + "wins", Toast.LENGTH_LONG).show();
-                        over=true;
+                        over = true;
                     }
 
 
                 }
             });
-            reset=(Button)view.findViewById(R.id.reset);
+            reset = (Button) view.findViewById(R.id.reset);
             reset.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    over=false;
-                    count=0;
+                    over = false;
+                    count = 0;
                     Osteps.clear();
                     Xsteps.clear();
                     allSteps.clear();
@@ -94,15 +95,17 @@ public class MainActivityFragment extends Fragment {
         }
         return view;
     }
-    private void initAdapter(){
+
+    private void initAdapter() {
         ArrayList list = new ArrayList();
-        for (int i = 0; i < Math.pow(numCols,2); i++) {
+        for (int i = 0; i < Math.pow(numCols, 2); i++) {
             list.add("");
         }
         adapter = new ArrayAdapter(getActivity(), R.layout.item, R.id.item, list);
 
 
     }
+
     private boolean isLine(String sel, int x, int y) {
         int[] p1 = {x, y};
         int[] p2;
@@ -115,12 +118,12 @@ public class MainActivityFragment extends Fragment {
         }
         for (int i = 0; i < lineStep.size() - 1; i++) {
             p2 = lineStep.get(i);
-            double l12=Math.pow(p1[0] - p2[0],2)+ Math.pow(p1[1] - p2[1],2);
-            if (l12==1||l12==2) {
-                List<int[]> newLineStep=new ArrayList<>();
+            double l12 = Math.pow(p1[0] - p2[0], 2) + Math.pow(p1[1] - p2[1], 2);
+            if (l12 == 1 || l12 == 2) {
+                List<int[]> newLineStep = new ArrayList<>();
                 newLineStep.addAll(lineStep);
                 newLineStep.remove(i);
-                if (lineInNum(p1,p2,newLineStep,2)){
+                if (lineInNum(p1, p2, newLineStep, 2)) {
                     return true;
                 }
             }
@@ -129,39 +132,38 @@ public class MainActivityFragment extends Fragment {
     }
 
     /**
-     *
-     * @param p1 顶点1
-     * @param p2 顶点2
+     * @param p1       顶点1
+     * @param p2       顶点2
      * @param lineStep
      * @param count
      * @return
      */
-    private boolean lineInNum(int[] p1,int[] p2,List<int[]> lineStep,int count){
+    private boolean lineInNum(int[] p1, int[] p2, List<int[]> lineStep, int count) {
 
-        for (int i=0;i<lineStep.size()-1;i++) {
-            int[] p3=lineStep.get(i);
-            double l23=Math.pow(p2[0] - p3[0], 2)+ Math.pow(p2[1] - p3[1], 2);
-            double l13=Math.pow(p1[0] - p3[0], 2)+ Math.pow(p1[1] - p3[1], 2);
+        for (int i = 0; i < lineStep.size() - 1; i++) {
+            int[] p3 = lineStep.get(i);
+            double l23 = Math.pow(p2[0] - p3[0], 2) + Math.pow(p2[1] - p3[1], 2);
+            double l13 = Math.pow(p1[0] - p3[0], 2) + Math.pow(p1[1] - p3[1], 2);
             if ((p1[0] - p3[0]) * (p2[1] - p3[1]) == (p2[0] - p3[0]) * (p1[1] - p3[1])) {
-                if (l13==1||l13==2){
+                if (l13 == 1 || l13 == 2) {
                     count++;
-                    if (count==numInLine){
+                    if (count == numInLine) {
                         return true;
-                    }else {
-                        List<int[]> nextLineStep=new ArrayList<>();
+                    } else {
+                        List<int[]> nextLineStep = new ArrayList<>();
                         nextLineStep.addAll(lineStep);
                         nextLineStep.remove(i);
-                        return lineInNum(p3,p2,nextLineStep,count);
+                        return lineInNum(p3, p2, nextLineStep, count);
                     }
-                }else if (l23==1||l23==2){
+                } else if (l23 == 1 || l23 == 2) {
                     count++;
-                    if (count==numInLine){
+                    if (count == numInLine) {
                         return true;
-                    }else {
-                        List<int[]> nextLineStep=new ArrayList<>();
+                    } else {
+                        List<int[]> nextLineStep = new ArrayList<>();
                         nextLineStep.addAll(lineStep);
                         nextLineStep.remove(i);
-                        return lineInNum(p1,p3,nextLineStep,count);
+                        return lineInNum(p1, p3, nextLineStep, count);
                     }
                 }
             }
